10306539f36fbd3fb3b241576061bece2c0b1510
HubertKuch <kuchhubert@gmail.com>
2023-02-24 22:38:12 +0100

    Init

---
+++ src/main/java/com/hubert/ovhmailcreator/OvhMailCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.hubert.ovhmailcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvhMailCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvhMailCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hubert/ovhmailcreator/OvhMailCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubert.ovhmailcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvhMailCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
